6255f0dbb5e16eb6c9b12d4516ef13cd797afc62
Tristan Vigar <tristanvigar@protonmail.com>
2019-12-11 10:45:38 -0700

    Pre-release bug fix: Missing /zip from download_zip() url

--- thingiverse_scraper.py
+++ thingiverse_scraper.py
@@ -65,7 +65,7 @@ def download_zip(current_page, url, html_title):
     time.sleep(sleep_timer_seconds)
     local_filepath = f'{download_directory}{current_page}-{html_title}.zip'
     chunk_iterations = 0
-    with requests.get(url, stream=True) as remote_zip_file:
+    with requests.get(url + '/zip', stream=True) as remote_zip_file:
         remote_zip_file.raise_for_status()
         with open(local_filepath, 'wb') as local_zip_file:
             for chunk in remote_zip_file.iter_content(chunk_size=remote_zip_buffer_size):
