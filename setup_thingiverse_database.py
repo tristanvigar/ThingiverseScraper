3a2bc0db9de2d41735366d3ff99757dbb581c9a4
Tristan Vigar <tristanvigar@protonmail.com>
2020-11-25 14:27:56 -0700

    Modified: Complete application overhaul plus added SQLite3 as database manager

--- setup_thingiverse_database.py
+++ setup_thingiverse_database.py
@@ -6,6 +6,13 @@ import sqlite3
 
 
 #
+# Configuration Information
+#
+
+thingiverse_table_name = 'thingiverse_catalog'
+
+
+#
 # Function Definitions
 #
 
@@ -56,27 +63,6 @@ def create_thingiverse_table(database_connection, database_cursor, table_name):
     database_connection.commit()
 
 
-def insert_dummy_record_into_thingiverse_table(database_connection, database_cursor, table_name):
-    # Add a dummy record with page number 0 to act as a starting point for thingiverse_scraper.py
-    record_id = None
-    page_number = 0
-    page_title = 'dummy_record'
-    datestamp = None
-
-    sql = f'''INSERT INTO {table_name} (id, page_number, page_title, datestamp) VALUES (?, ?, ?, ?)'''
-
-    database_cursor.execute(sql, (record_id, page_number, page_title, datestamp))
-
-    database_connection.commit()
-
-
-#
-# Configuration Information
-#
-
-table_name = 'thingiverse_catalog'
-
-
 #
 # Start Here
 #
@@ -85,8 +71,7 @@ sqlite_database_path = get_sqlite_database_path()
 database_connection = get_database_connection(sqlite_database_path)
 database_cursor = get_database_cursor(database_connection)
 
-create_thingiverse_table(database_connection, database_cursor, table_name)
-insert_dummy_record_into_thingiverse_table(database_connection, database_cursor, table_name)
+create_thingiverse_table(database_connection, database_cursor, thingiverse_table_name)
 
 close_database_cursor(database_cursor)
 close_database_connection(database_connection)
